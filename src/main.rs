7a9bad1577b6074f21e59b20abf64e2c15493ead
zokhcat <understandingzak@gmail.com>
2024-09-24 20:20:00 +0530

    feat(url): Scrape HTML content in URL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+pub mod modules;
+
+use crate::modules::url_scrape::url_scrape;
+
 fn main() {
-    println!("Hello, world!");
+
+    let html_content = url_scrape();
+
+    println!("{}", html_content);
 }
